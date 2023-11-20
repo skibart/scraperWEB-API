edc4608a7f5fe9ebf711fa85ffe44d2b6c4903d8
unknown <kalinowskibartosz@gmail.com>
2023-11-20 11:21:08 +0100

    add cors

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,18 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cron_1 = require("./util/cron");
 const morgan_1 = __importDefault(require("morgan"));
 const ping_1 = __importDefault(require("./util/ping"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 app.use((0, morgan_1.default)(':method :url :status :res[content-length] - :response-time ms'));
+app.use((0, cors_1.default)());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 app.get('/', (req, res) => {
     res.send('Hello!');
 });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { cronJobs } from './util/cron';
 import morgan from 'morgan';
 import fetchZieleniec from './resorts/zielieniec';
 import pingServer from './util/ping';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +14,14 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello!');
