0657358085915768f01f546f4db4e1632d43b76d
unknown <kalinowskibartosz@gmail.com>
2023-11-20 10:59:03 +0100

    change api return json

--- src/mongodb/getFromMongoDb.ts
+++ src/mongodb/getFromMongoDb.ts
@@ -6,7 +6,7 @@ async function getFromMongoDb(collectionName: string) {
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
-    const lastDocument = await collection.find().sort({ $natural: -1 }).limit(2).toArray();
+    const lastDocument = await collection.find().sort({ $natural: -1 }).limit(1).toArray();
     // await client.db('admin').command({ ping: 1 });
     // console.log('Pinged your deployment. You successfully connected to MongoDB!');
 
